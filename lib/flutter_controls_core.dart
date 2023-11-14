c3358160796e3498f2228f482d584dc1ce87ee8b
Rich Neswold <neswold@fnal.gov>
2023-11-14 14:20:42 -0600

    :sparkles: import the `DeviceValue` code from param page

--- lib/flutter_controls_core.dart
+++ lib/flutter_controls_core.dart
@@ -1,1 +1,3 @@
 library flutter_controls_core;
+
+export "src/device_values.dart";
---
+++ lib/src/device_values.dart
@@ -0,0 +1,90 @@
+/// Defines all types used by devices in Fermilab's control system.
+///
+/// This module providess a sealed class hierarchy which defines all the types
+/// of values that can be found in Fermilab's control system. If a new type is
+/// added, it needs to be defined in this module and a new release of this
+/// package needs to be made.
+///
+/// Functions that deal with devices will specify `DeviceValue` as a parameter
+/// or return a `DeviceValue` value. For instance, if a hypothetical `setDevice`
+/// function takes a devices name and a device value, we could set it to `10.0`
+/// by making the call
+///
+/// ```
+/// setDevice("DEVICE", DevScalar(10.0));
+/// ```
+///
+/// If one were receiving a value from a function, you can use pattern-matching
+/// to get the value. For instance, reading (hypothetically) outdoor temperature
+/// returns a floating point number:
+///
+/// ```
+/// if (readDevice("M:OUTTMP") case DevScalar(value: deg)) {
+///     print("It is $deg degrees outside.");
+/// } else {
+///     print("M:OUTTMP didn't return a floating point number!");
+/// }
+/// ```
+
+library device_values;
+
+/// Use this type to indicate any device type is allowed.
+///
+/// Functions that use this as a parameter will accept any of the derived
+/// classes. Functions that return this type can, actually, return any of the
+/// derived types.
+
+sealed class DeviceValue {
+  const DeviceValue();
+}
+
+/// Represents a raw, byte array.
+///
+/// If you ask for "raw" data from a device, you'll get an instance of this
+/// type. Your application will have to interpret the data and determine how to
+/// scale it. This type is mostly useful for diagnostics or for actual, binary
+/// data buffers (i.e. image data.)
+
+class DevRaw extends DeviceValue {
+  final List<int> value;
+
+  const DevRaw(this.value);
+}
+
+/// Represents a single, floating point number.
+///
+/// Most devices deal with floating point values. This is the type to send to
+/// a device and what would be returned when reading it.
+
+class DevScalar extends DeviceValue {
+  final double value;
+
+  const DevScalar(this.value);
+}
+
+/// Represents an array of floating point values.
+///
+/// Some devices are "array devices" and will return an array of floating point
+/// values. This type is also used by EPICS "waveform" devices.
+
+class DevScalarArray extends DeviceValue {
+  final List<double> value;
+
+  const DevScalarArray(this.value);
+}
+
+/// Represents a single string of characters.
+
+class DevText extends DeviceValue {
+  final String value;
+
+  const DevText(this.value);
+}
+
+/// Represents an array of strings.
+
+class DevTextArray extends DeviceValue {
+  final List<String> value;
+
+  const DevTextArray(this.value);
+}
