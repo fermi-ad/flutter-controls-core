3cf2418f3a28e887ff3cfae87f4400e436e51f66
John Diamond <jdiamond@fnal.gov>
2023-12-06 16:01:10 -0600

    Added fermi theme generated with the Material Design theme builder

--- lib/src/app_scaffold.dart
+++ lib/src/app_scaffold.dart
@@ -3,54 +3,18 @@
 library app_scaffold;
 
 import 'package:flutter/material.dart';
+import 'package:flutter_controls_core/src/fermi_theme.dart';
 import 'package:go_router/go_router.dart';
 
-// Our text theme taken from
-// https://codingwitht.com/how-to-use-theme-in-flutter-light-dark-theme/
-
-class _GlobalTextTheme {
-  _GlobalTextTheme._();
-
-  static TextTheme globalTextTheme = const TextTheme(
-    titleSmall: TextStyle(fontSize: 16.0),
-    titleMedium: TextStyle(fontSize: 18.0),
-    titleLarge: TextStyle(fontSize: 24.0),
-    bodySmall: TextStyle(fontSize: 14.0),
-    bodyMedium: TextStyle(fontSize: 18.0),
-    bodyLarge: TextStyle(fontSize: 20.0),
-  );
-
-  static TextTheme lightTextTheme = globalTextTheme.copyWith();
-  static TextTheme darkTextTheme = globalTextTheme.copyWith();
-}
-
-// Our app theme taken from
-// https://codingwitht.com/how-to-use-theme-in-flutter-light-dark-theme/
+// Our Fermi theme generated with - https://m3.material.io/theme-builder#/custom
 
 class _GlobalAppTheme {
   _GlobalAppTheme._();
 
-  static ThemeData lightTheme = ThemeData(
-    primarySwatch: Colors.blue,
-    brightness: Brightness.light,
-    textTheme: _GlobalTextTheme.lightTextTheme,
-    appBarTheme: const AppBarTheme(),
-    floatingActionButtonTheme: const FloatingActionButtonThemeData(),
-    elevatedButtonTheme:
-        ElevatedButtonThemeData(style: ElevatedButton.styleFrom()),
-    useMaterial3: true,
-  );
-
-  static ThemeData darkTheme = ThemeData(
-    primarySwatch: Colors.blue,
-    brightness: Brightness.dark,
-    textTheme: _GlobalTextTheme.darkTextTheme,
-    appBarTheme: const AppBarTheme(),
-    floatingActionButtonTheme: const FloatingActionButtonThemeData(),
-    elevatedButtonTheme:
-        ElevatedButtonThemeData(style: ElevatedButton.styleFrom()),
-    useMaterial3: true,
-  );
+  static ThemeData lightTheme =
+      ThemeData(useMaterial3: true, colorScheme: lightColorScheme);
+  static ThemeData darkTheme =
+      ThemeData(useMaterial3: true, colorScheme: darkColorScheme);
 }
 
 /// Creates an application scaffold that uses the `GoRouter` package.
