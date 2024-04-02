d0a4ed13b915a159853f9c63ce941318ecd0b6ed
Rich Neswold <neswold@fnal.gov>
2024-04-02 16:50:12 -0500

    :recycle: get minimal authentication working
    This gets some initial authentication support into `-core`. We definitely need to add features
    (like being able to log in and out from the app) but this gives us a foundation to work on.

--- lib/src/app_scaffold.dart
+++ lib/src/app_scaffold.dart
@@ -3,8 +3,9 @@
 library app_scaffold;
 
 import 'package:flutter/material.dart';
-import 'package:flutter_controls_core/src/fermi_theme.dart';
 import 'package:go_router/go_router.dart';
+import 'fermi_theme.dart';
+import 'auth_widget.dart';
 
 // Our Fermi theme generated with - https://m3.material.io/theme-builder#/custom
 
